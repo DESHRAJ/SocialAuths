3763d3bd83ebfb04f6c66b178eecc3ba102c0749
Deshraj <deshrajdry@gmail.com>
2015-03-05 00:26:58 +0530

    changes made in urls and views

--- auths/views.py
+++ auths/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import * 
 from django.shortcuts import render_to_response
+from django.template import Context,RequestContext
+
 # Create your views here.
 def home(request):
 	''' View for the home page of the application'''
@@ -9,4 +11,4 @@ def home(request):
 	return render_to_response('index.html')
 
 def dashboard(request):
-	return render_to_response('dashboard.html')
+	return render_to_response('dashboard.html',{'user':request.user},context_instance = RequestContext(request))
--- socialAuth/urls.py
+++ socialAuth/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'', include('auths.urls')),
+    (r'logout', 'django.contrib.auth.views.logout',{'next_page': '/'}),
     (r'', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
