2112b1684eef4a9eb3641dd13f2537ab8082ff33
Deshraj <deshrajdry@gmail.com>
2015-03-08 19:44:58 +0530

    jobid done and image search complete

--- auths/views.py
+++ auths/views.py
@@ -15,55 +15,81 @@ import simplejson
 import cStringIO
 import Image
 # Create your views here.
+
 def home(request):
 	''' View for the home page of the application'''
 	if request.user.is_authenticated():
 		return HttpResponseRedirect('/dashboard')
 	return render_to_response('index.html')
 
-# @login_required	
 def dashboard(request):
 	''' View of Dashboard page for user after logging in using the Social Account '''
 	return render_to_response('dashboard.html',{'user':request.user},context_instance = RequestContext(request))
 
-# @login_required
 def trainModel(request):
 	''' View for training the model after fetching the results from google/flickr etc'''
-	if request.user.is_authenticated():
-		if request.GET:
-			category = request.GET.get('category')
-			category = category.lower()
-			# os.makedirs('/'+request.user.email+'/train/'+category)
-			# os.makedirs('/'+request.user.email+'/test')
-			fetchFromGoogle(category)
-		return render_to_response('train.html',{'user':request.user},context_instance=RequestContext(request))
-	return HttpResponseRedirect('/')
+	if not request.user.is_authenticated():
+		# if "userId" not in request.session:
+		# 	if not request.session.exists(request.session.session_key):
+		# 		request.session.create()
+		# 		print "NEW SESSION IS CREATED"
+		# 	request.session['userId']=request.session._session_key
+		# print "THE SESSION HAS BEEN CREATED WITH VALUE",request.session['userId'] 
+		# jobId = request.session['userId']
+		jobId = createSession(request)
+	else:
+		jobId = request.session._session_key
+	print "JOBID IS ",jobId
+	if request.is_ajax():
+		print "SUCCESS AJAX CALL"
+		category = request.GET.get('category',None)
+		category= category.split()
+		category='+'.join(category)
+		fetchFromGoogle(category,jobId)
+	return render_to_response('train.html',{'user':request.user},context_instance=RequestContext(request))
 
-def fetchFromGoogle(searchTerm):
-	''' Function for fetching 20 images using the google apis'''
+def fetchFromGoogle(searchTerm,jobId):
+	''' Function for fetching top 24 Google Images using the google apis'''
+	directory = "/home/dypy/Pictures/cloudcv/"+jobId
+	print "DIRETORY CREATION STARTS"
+	if not os.path.exists(directory):
+		os.makedirs(directory+"/test")
+		os.makedirs(directory+"/utils")
+	if not os.path.exists(directory+"/train/"+searchTerm):
+		os.makedirs(directory+"/train/"+searchTerm)
+		print "DIRECTORY CREATION SUCCESSFUL"
 	fetcher = urllib2.build_opener()
-	startIndex = 0
-	i,urls=1,1
-	while(i<=20):
-		flag=0
-		searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + str(searchTerm)+'&rsz=8'+'&start='+str(i)
+	icount=-1
+	flag = 0
+	image_type = "image"
+	while(icount<=24):
+		searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + str(searchTerm)+'&rsz=8'+'&start='+str(icount+1)
 		print searchUrl
 		f = fetcher.open(searchUrl)
 		deserialized_output = simplejson.load(f)
 		for img in deserialized_output['responseData']['results']:
-			if urls>=20:
-				flag=1
-				break
-			else:
-				file = cStringIO.StringIO(urllib2.urlopen(img['unescapedUrl']).read())
-				img = Image.open(file)
-				directory = "/home/dypy/Pictures/cloudcv/"+str(searchTerm)
-				if not os.path.exists(directory):
-					os.makedirs(directory)
-				img.save(directory+"/image"+str(urls)+".jpg", format="JPEG")
-				# urls.append(img['unescapedUrl'])
-				# print "$$$$$$$$$$", "          ", len(urls)
-				urls+=1
-		if flag==1:
+			try:
+				if img['unescapedUrl'][-3:]!="gif":
+					file = cStringIO.StringIO(urllib2.urlopen(img['unescapedUrl']).read())
+					img = Image.open(file)
+					urls = len([i for i in os.listdir(directory+"/train/"+searchTerm) if image_type in i]) + 1
+					img.save(directory+"/train/"+searchTerm+"/image"+str(urls)+".jpg", format="JPEG")
+					if urls==20:
+						flag=1
+						break
+			except:
+				print "$$$$$$$ LOG $$$$$$$"
+				print img['unescapedUrl'],urls
+		if flag:
 			break
-		i+=8
+		icount+=8
+
+def createSession(request):
+	if "userId" not in request.session:
+		if not request.session.exists(request.session.session_key):
+			request.session.create()
+			print "NEW SESSION IS CREATED"
+		request.session['userId']=request.session._session_key
+	print "THE SESSION HAS BEEN CREATED WITH VALUE",request.session['userId'] 
+	jobId = request.session['userId']
+	return jobId
